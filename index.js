0a15e75f8ad6e91bc828ba3bbf7ed0d574dbd4b3
杨君华 <1025357509@qq.com>
2018-08-22 11:36:46 +0800

    refactor: 代码优化重构，增加git init初始化执行命令

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ const program = require('commander')
 const utils = require('./utils')
 
 program
-  .version('0.0.1', '-v, --version')
+  .version('1.0.1', '-v, --version')
   .command('init <projectName>')
   .action(async projectName => {
-    const isGenerate = await utils.confirmIsGenerate(projectName)
-
-    if (!isGenerate) { return }
-
-    const packageInfo = await utils.getPackageInfo(projectName)
-    utils.createProject(packageInfo, utils.updateReadme)
+    await utils.init(projectName)
   })
 
 program.parse(process.argv)
--- utils.js
+++ utils.js
@@ -8,142 +8,202 @@ const chalk = require('chalk')
 const handlebars = require('handlebars')
 const { exec, execSync } = require('child_process')
 
-exports.confirmIsGenerate = function(projectName) {
-  let message = ''
-  const projectPath = path.resolve(process.cwd(), projectName)
+module.exports = {
+  async init(projectName) {
+    const isGenerate = await this.confirmIsGenerate(projectName)
+    if (!isGenerate) { return }
 
-  if (fs.existsSync(projectPath) && fs.statSync(projectPath).isDirectory()) {
-    message = 'Target directory exists. Continue?'
-  }
-  if (!message) {
-    return Promise.resolve(true)
-  }
+    const packageInfo = await this.collectPackageInfo(projectName)
+    const { dirname } = packageInfo
+    const spinner = ora('downloading template').start()
 
-  return new Promise(resolve => {
-    inquirer
-      .prompt([
-        {
-          type: 'confirm',
-          name: 'isGenerate',
-          default: true,
-          message
-        }
-      ])
-      .then(answers => {
-        if (answers['isGenerate']) {
-          resolve(true)
-        } else {
-          resolve(false)
-        }
-      })
-  })
-}
-
-exports.getPackageInfo = function(projectName) {
-  let userInfo = ''
-  try {
-    const userName = execSync('git config user.name', { encoding: 'utf8' }).trim()
-    const userEmail = execSync('git config user.email', { encoding: 'utf8' }).trim()
-    userInfo = `${userName} \<${userEmail}\>`
-  } catch (error) {}
-
-  return new Promise(resolve => {
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'projectName',
-          message: 'Project name',
-          default: projectName
-        },
-        {
-          type: 'input',
-          name: 'description',
-          message: 'Project description',
-          default: 'A Vue.js component project'
-        },
-        {
-          type: 'input',
-          name: 'author',
-          message: 'Author',
-          default: userInfo
-        },
-        {
-          type: 'input',
-          name: 'repositoryUrl',
-          message: 'repository url',
-          default: ''
-        }
-      ])
-      .then(answers => {
-        const { projectName, description, author, repositoryUrl } = answers
-        resolve({
-          dirname: projectName,
-          name: projectName.replace(/^kkl-?/i, ''),
-          description,
-          author,
-          repository: {
-            url: repositoryUrl
-          }
-        })
-      })
-  })
-}
-
-exports.createProject = function(packageInfo) {
-  const { dirname } = packageInfo
-  const spinner = ora('downloading template').start()
-
-  try {
-    shell.rm('-rf', path.resolve(process.cwd(), dirname))
-  } catch (error) {
-     //... 
-  }
-
-  exec(`git clone https://github.com/yjh30/vue-ssr-component-tpl.git ${dirname}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log(chalk.red(`downloading template fail ${error}`))
-      spinner.stop()
-      return
+    try {
+      shell.rm('-rf', path.resolve(process.cwd(), dirname))
+    } catch (error) {
+       //... 
     }
 
-    spinner.succeed('downloading template succeed')
+    const downloadPromise = this.downloadTemplate(packageInfo)
 
-    const packageFileName = `${dirname}/package.json`
+    downloadPromise.catch(error => {
+      console.log(chalk.red(`\ndownloading template fail ${error}`))
+      spinner.stop()
+    })
+
+    await downloadPromise
+
+    spinner.succeed('downloading template succeed')
+      console.log(`
+# ${chalk.green('Project initialization finished!')}\n
+# ========================\n
+To get started:\n${chalk.yellow(`cd ${dirname}\nnpm install (or if using yarn: yarn)\nnpm run dev\n`)}
+      `)
+
+    this.updatePackageJson(packageInfo)
+    this.updateReadme(packageInfo)
+    this.gitInit(packageInfo)
+  },
+
+  /**
+   * confirmIsGenerate 确定是否生成项目
+   * @param  {String} projectName 用户输入的项目名称
+   * @return {Promise}
+   */
+  confirmIsGenerate(projectName) {
+    let message = ''
+    const projectPath = path.resolve(process.cwd(), projectName)
+
+    if (fs.existsSync(projectPath) && fs.statSync(projectPath).isDirectory()) {
+      message = 'Target directory exists. Continue?'
+    }
+    if (!message) {
+      return Promise.resolve(true)
+    }
+
+    return new Promise(resolve => {
+      inquirer
+        .prompt([
+          {
+            type: 'confirm',
+            name: 'isGenerate',
+            default: true,
+            message
+          }
+        ])
+        .then(answers => {
+          if (answers['isGenerate']) {
+            resolve(true)
+          } else {
+            resolve(false)
+          }
+        })
+    })
+  },
+
+  /**
+   * collectPackageInfo 收集包字段信息
+   * @param  {String} projectName 用户输入的项目名称
+   * @return {Promise}
+   */
+  collectPackageInfo(projectName) {
+    let userInfo = ''
+    try {
+      const userName = execSync('git config user.name', { encoding: 'utf8' }).trim()
+      const userEmail = execSync('git config user.email', { encoding: 'utf8' }).trim()
+      userInfo = `${userName} \<${userEmail}\>`
+    } catch (error) {}
+
+    return new Promise(resolve => {
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'projectName',
+            message: 'Project name',
+            default: projectName
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Project description',
+            default: 'A Vue.js component project'
+          },
+          {
+            type: 'input',
+            name: 'author',
+            message: 'Author',
+            default: userInfo
+          },
+          {
+            type: 'input',
+            name: 'repositoryUrl',
+            message: 'repository url',
+            default: ''
+          }
+        ])
+        .then(answers => {
+          const { projectName, description, author, repositoryUrl } = answers
+          resolve({
+            dirname: projectName,
+            name: projectName.replace(/^kkl-?/i, ''),
+            description,
+            author,
+            repository: {
+              url: repositoryUrl
+            }
+          })
+        })
+    })
+  },
+
+  /**
+   * downloadTemplate 下载组件模版
+   * @param  {Object} packageInfo 包信息
+   * @return {Promise}
+   */
+  downloadTemplate(packageInfo) {
+    return new Promise((resolve, reject) => {
+      exec(`git clone https://github.com/yjh30/vue-ssr-component-tpl.git ${packageInfo.dirname}`, (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve()
+        }
+      })
+    })
+  },
+
+  /**
+   * updatePackageJson 更新package.json文件
+   * @param  {Object} packageInfo 包信息
+   * @return {undefined}
+   */
+  updatePackageJson(packageInfo) {
+    const packageFileName = `${packageInfo.dirname}/package.json`
     const packageJson = fs.readFileSync(packageFileName).toString()
     fs.writeFileSync(packageFileName, handlebars.compile(packageJson)(packageInfo))
+  },
 
+  /**
+   * updateReadme 更新README.md文档
+   * @param  {Object} packageInfo 包信息
+   * @return {undefined}
+   */
+  updateReadme(packageInfo) {
+    const { dirname, name } = packageInfo
+    const readmePath = path.resolve(process.cwd(), `${dirname}/README.md`)
+    if (fs.existsSync(readmePath)) {
+      shell.rm('-rf', readmePath)
+    }
+
+    const context = `
+  # @kkl/${name}
+  > 用一句话概况组件（如：vue移动端h5 水平滑屏手势交互组件）
+  ### 安装
+  \`\`\` bash
+  npm i -S @kkl/${name}
+  \`\`\`
+  ### 使用
+    `
+
+    fs.writeFileSync(readmePath, context)
+  },
+
+  /**
+   * gitInit 执行git init，服务于第三方githooks或husky模块
+   * @param  {Object} packageInfo 包信息
+   * @return {undefined}
+   */
+  gitInit(packageInfo) {
+    const { dirname } = packageInfo
     try {
       shell.rm('-rf', path.resolve(process.cwd(), `${dirname}/.git`))
     } catch (error) {
        //... 
     }
 
-    console.log(`
-# ${chalk.green('Project initialization finished!')}\n
-# ========================\n
-To get started:\n${chalk.yellow(`cd ${dirname}\nnpm install (or if using yarn: yarn)\nnpm run dev\n`)}
-    `)
-
-    exports.updateReadme(packageInfo)
-  })
-}
-
-exports.updateReadme = function(packageInfo) {
-  const readmePath = path.resolve(process.cwd(), `${packageInfo.dirname}/README.md`)
-  if (fs.existsSync(readmePath)) {
-    shell.rm('-rf', readmePath)
+    process.chdir(`${process.cwd()}/${dirname}`)
+    execSync('git init')
+    process.chdir(path.resolve(__dirname, ''))
   }
-
-  const context = `
-# @kkl/${packageInfo.name}
-> 用一句话概况组件（如：vue移动端h5 水平滑屏手势交互组件）
-### 安装
-\`\`\` bash
-npm i -S @kkl/${packageInfo.name}
-\`\`\`
-### 使用
-  `
-
-  fs.writeFileSync(readmePath, context)
 }
